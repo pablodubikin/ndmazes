6482af92876ab6e01cbf102e244b96a179dded9e
onz <pab@MacBook-Pro-de-Pablo.local>
2019-12-04 01:20:06 +0100

    Revert "Recovering all files"
    This reverts commit d366648cc2a1432cfafc704720caf176a65375bb.

--- clear_screen.hpp
+++
@@ -1,8 +0,0 @@
-// Source: http://www.cplusplus.com/articles/4z18T05o/#POSIX
-// Altered by onz
-#ifndef CLEARSCREEN_H
-#define CLEARSCREEN_H
-
-void ClearScreen();
-
-#endif 
--- ipair.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef IPAIR_h
-#define IPAIR_h
-#include<bits/stdc++.h>
-// Pair of ints
-typedef std::pair<int, int> iPair;
-#endif
